b2c3354088fdcf88da85a73275051397f187639c
Andrei Picus <office.nightcrawler@gmail.com>
2014-06-30 13:27:13 +0300

    Convert tabs to spaces.

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-	$(".fs").fsortable({
-		connectWith: ".fs:not(.full)",
-		tolerance: "pointer",
-		helper: "clone",
-		appendTo: "body",
-	}).disableSelection();
+    $(".fs").fsortable({
+        connectWith: ".fs:not(.full)",
+        tolerance: "pointer",
+        helper: "clone",
+        appendTo: "body",
+    }).disableSelection();
 
-	$("#content .item").draggable({
-		connectToSortable: ".fs:not(.full)",
-		revert: "invalid",
-		helper: "clone"
-	});
+    $("#content .item").draggable({
+        connectToSortable: ".fs:not(.full)",
+        revert: "invalid",
+        helper: "clone"
+    });
 
-  $(".fs").on("sortupdate", function() {
-      $(this).attr("data-content", $(this).fsortable("capacity"));
-  }).each(function() {
-      // Set the initial value of the attribute.
-      $(this).attr("data-content", $(this).find(".fsortable-empty").length);
-  });
+    $(".fs").on("sortupdate", function() {
+        $(this).attr("data-content", $(this).fsortable("capacity"));
+    }).each(function() {
+        // Set the initial value of the attribute.
+        $(this).attr("data-content", $(this).find(".fsortable-empty").length);
+    });
 });
 
